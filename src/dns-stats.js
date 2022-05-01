89446a84f3af8d8ea05f647c836c98bc87303632
Oksana <seregina.oksana.1987@gmail.com>
2022-05-01 19:28:34 +0300

    feat: add the dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const object = {};
+
+  domains.forEach((elem) => {
+    elem
+      .split(".")
+      .reverse()
+      .reduce((res, el) => {
+        res = res + `.${el}`;
+        if (object[res]) {
+          object[res]++;
+        } else {
+          object[res] = 1;
+        }
+        return res;
+      }, "");
+  });
+  return object;
 }
 
+console.log(getDNSStats(["code.yandex.ru", "music.yandex.ru", "yandex.ru"]));
+
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
